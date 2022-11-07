09213d6c129e8c3a7511c52f28e39c8e8959901d
Sangeeta Dagar <sangeeta_dagar@epam.com>
2022-11-08 01:42:51 +0530

    integrate-sqs-sns

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://igkts029m2.execute-api.us-west-2.amazonaws.com/dev",
-  bff: "https://21omv24q73.execute-api.us-west-2.amazonaws.com/dev",
+  import: "https://nh4zu4aewa.execute-api.us-west-2.amazonaws.com/dev",
+  bff: "https://31se4a8q61.execute-api.us-west-2.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
